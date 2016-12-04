4f2cecf7308becfd9f34734f24ee5605dd5ed388
Robbie McKinstry <rem89@pitt.edu>
2016-12-04 03:40:52 -0500

    Getting everything set up with flow

---
+++ src/router.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+import Home from './components/home';
+import Index from './components/index';
+import ProfilePage from './components/profilePage';
+
+import { Router, browserHistory } from 'react-router';
+import routes from './router'
+
+export default(
+    <Router history={browserHistory} routes={routes} >
+        <Route path="/"         component={ Index } />
+        <Route path="/home"     component={ Home } />
+        <Route path="/profiles" component={ ProfilePage } />
+    </Router>
+);
