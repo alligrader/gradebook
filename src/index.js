d81c5b3258bec32e2077b2bf18d4bfb7e225a2a9
Robbie McKinstry <rem89@pitt.edu>
2016-12-04 03:40:52 -0500

    Getting everything set up with flow

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
+// @flow
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
-import App from './components/app';
 import reducers from './reducers';
+import router from './router'
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    { router }
   </Provider>
   , document.querySelector('.container'));
