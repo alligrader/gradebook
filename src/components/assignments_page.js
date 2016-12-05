851fdf3ba45a7fec2fbaeb54962df0cd264201f1
Robbie McKinstry <rem89@pitt.edu>
2016-12-05 02:06:12 -0500

    Adding enumerations for route paths and empty files for new paths

---
+++ src/components/assignments_page.js
@@ -0,0 +1,7 @@
+// @flow
+
+import React from 'react';
+
+export default () => {
+    return <div>This is the assignments page.</div>
+};
--- src/router.js
+++ src/router.js
@@ -1,18 +1,48 @@
 // @flow
 
 import React from 'react';
-import { Route, IndexRoute } from 'react-router';
-import Home from './components/home';
-import Index from './components/index';
-import ProfilePage from './components/profilePage';
+import { Route, IndexRoute, Router, browserHistory } from 'react-router';
+import {Enum} from 'enumify';
 
-import { Router, browserHistory } from 'react-router';
-import routes from './router'
+import HomePage from './components/home_page';
+import IndexPage from './components/index_page';
+import ProfilePage from './components/profile_page';
+import CoursesPage from './components/courses_page';
+import AssignmentsPage from './components/assignments_page';
+
+// This guy gives us a nice and pretty enum for debugging purposes. We should probably just this into a helper file so other pages can import it to create links.
+class Path extends Enum {}
+Path.initEnum({
+    index: {
+        get path() { return '/'; },
+    },
+
+    home: {
+        get path() { return '/home'; },
+    },
+
+    profiles: {
+        get path() { return '/profiles'; },
+    },
+
+    courses: {
+        get path() { return '/courses'; },
+    },
+
+    assignments: {
+        get path() { return '/assignments'; },
+    },
+
+});
 
 export default(
-    <Router history={browserHistory} routes={routes} >
-        <Route path="/"         component={ Index } />
-        <Route path="/home"     component={ Home } />
-        <Route path="/profiles" component={ ProfilePage } />
+    <Router history={ browserHistory } >
+
+        <Route path={ Path.index.path       }  component={ IndexPage } />
+        <Route path={ Path.home.path        }  component={ HomePage } />
+        <Route path={ Path.profiles.path    }  component={ ProfilePage } />
+        <Route path={ Path.courses.path     }  component={ CoursesPage } />
+        <Route path={ Path.assignments.path }  component={ AssignmentsPage } />
+
     </Router>
 );
