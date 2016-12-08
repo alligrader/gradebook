9ff34bfb242d8b301c303d381286d1e072747881
Robbie McKinstry <rem89@pitt.edu>
2016-12-08 01:45:44 -0500

    Using a redux-link tag instead; reduces how often we have to reload React.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,24 +1,21 @@
 // @flow
 
 import React from 'react';
-import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { Navbar, Nav, NavItem, Col } from 'react-bootstrap';
 import Paths from '../helpers/path_helper';
+import { Link } from 'react-router';
 
 export default () => {
     return (
         <div className="col-md-12">
             <Navbar>
-                <Navbar.Header>
-                    <Navbar.Brand>
-                    <a href={ Paths.home.path }>Alligrader</a>
-                    </Navbar.Brand>
-                </Navbar.Header>
                 <Nav>
-                    <NavItem eventKey={1} href={ Paths.profiles.path }>Bug Profiles</NavItem>
-                    <NavItem eventKey={2} href={ Paths.courses.path}>Courses</NavItem>
-                    <NavItem eventKey={3} href={ Paths.assignments.path}>Assignments</NavItem>
+                    <Col md={3}><Link to={ Paths.home.path }>Alligrader</Link></Col>
+                    <Col md={3}><Link to={ Paths.profiles.path }>Bug Profiles</Link></Col>
+                    <Col md={3}><Link to={ Paths.courses.path }>Courses</Link></Col>
+                    <Col md={3}><Link to={ Paths.assignments.path }>Assignments</Link></Col>
                 </Nav>
             </Navbar>
         </div>
-    )
+    );
 };
